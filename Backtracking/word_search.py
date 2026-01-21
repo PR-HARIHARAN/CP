74da484d56e0634802c3b67e3c73d5838526df31
Hariharan P R <145788914+PR-HARIHARAN@users.noreply.github.com>
2026-01-21 16:16:33 +0530

    Add problem link to word_search.py
    Added problem link for word search problem.

---
+++ Backtracking/word_search.py
@@ -0,0 +1,28 @@
+#Problem_link: https://leetcode.com/problems/word-search/description/
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        def backtrack(i,j,k):
+            if k == len(word):
+                return True
+            
+            if i<0 or i>=rows or j<0 or j>=cols or board[i][j]!=word[k]:
+                return False
+            
+            temp = board[i][j]
+            board[i][j] = ""
+
+            if backtrack(i+1,j,k+1) or backtrack(i-1,j,k+1) or backtrack(i,j+1,k+1) or backtrack(i,j-1,k+1):
+                return True
+
+            board[i][j] = temp
+            return False
+
+        for i in range(rows):
+            for j in range(cols):
+                if backtrack(i,j,0):
+                    return True
+        return False
+
+
+            
